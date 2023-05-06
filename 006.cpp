90e0fed941127177d9b8d5ca335b1741ed80a844
Rushikesh Chaure <113259190+rushi8903@users.noreply.github.com>
2023-05-07 03:12:48 +0530

    Add files via upload

---
+++ 006.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void takeArrayInput(int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << "Enter the arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+}
+
+int *reverseArray(int arr[], int len)
+{
+    int *reverseArr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        reverseArr[i] = arr[len - i - 1];
+    }
+    return reverseArr;
+}
+
+void displayArray(int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int len;
+    cout << "Enter the length of array :" << endl;
+    cin >> len;
+    int arr[len];
+    takeArrayInput(arr, len);
+    displayArray(arr, len);
+    int *reversedArr = reverseArray(arr, len);
+    cout << "Reversed array is : ";
+    displayArray(reversedArr, len);
+    return 0;
+}
+
